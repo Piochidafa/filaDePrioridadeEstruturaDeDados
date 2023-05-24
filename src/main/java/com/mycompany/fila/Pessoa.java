f444fa568f6d34fa85419988f47db12a5eeca3db
piochidafa <cauacastrooliveira@outlook.com>
2023-05-24 02:17:07 -0400

    Adicionando a terefa de fila no github

---
+++ src/main/java/com/mycompany/fila/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fila;
+
+/**
+ *
+ * @author Jubileu
+ */
+public class Pessoa {
+    
+    String nome;
+    int idade;
+    boolean prioridade = false;
+  
+    public Pessoa(String nome, Integer idade){
+    
+        this.nome = nome;
+        this.idade = idade;
+    }
+  
+    public boolean isPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(boolean prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
